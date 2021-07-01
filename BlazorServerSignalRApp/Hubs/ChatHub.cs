e8bd3a88f84a335c08f1d367cd209f82a767f6ed
ryan4gitee <rliang.lai@outlook.com>
2021-07-01 17:43:00 +0800

    Init Code

---
+++ BlazorServerSignalRApp/Hubs/ChatHub.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServerSignalRApp.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+        public async Task SendToCallerMessage(string user, string message)
+        {
+            var reply = $"System Reviced Your Message: {message}";
+            await Clients.Caller.SendAsync("ReceiveMessage", "", reply);
+        }
+
+        public async Task SendToGroupMessage(string user, string message, string groupName)
+        {
+            var reply = $"To Group {groupName}: {message}";
+            await Clients.Group(groupName).SendAsync("ReceiveMessage", user, reply);
+        }
+
+        public async Task AddToGroup(string user, string groupName)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+
+            await Clients.Group(groupName).SendAsync("ReceiveMessage", user, $"{user} has joined the group {groupName}.");
+        }
+
+        public async Task RemoveFromGroup(string user, string groupName)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupName);
+
+            await Clients.Group(groupName).SendAsync("ReceiveMessage", user, $"{user} has left the group {groupName}.");
+        }
+    }
+}
---
+++ WPF-SignalR/MainWindow.xaml.cs
@@ -0,0 +1,149 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_SignalR
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string userName;
+        private string group;
+
+        HubConnection connection;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            connection = new HubConnectionBuilder()
+                .WithUrl("http://localhost:9080/ChatHub")
+                .Build();
+
+            connection.Closed += async (error) =>
+            {
+                await Task.Delay(new Random().Next(0, 5) * 1000);
+                await connection.StartAsync();
+            };
+
+            group = $"GROUP{new Random().Next(0, 2)}";
+            userName = $"User:{new Random().Next(1000, 5000)}({group})";
+            txtUser.Text = userName;
+
+            connection.On<string, string>("ReceiveMessage", (user, message) =>
+            {
+                this.Dispatcher.Invoke(() =>
+                {
+                    var newMessage = $"{user}: {message}\r\n";
+                    rtxMessage.AppendText(newMessage);
+                    rtxMessage.ScrollToEnd();
+                });
+            });
+        }
+
+        private async void btnConnect_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                await connection.StartAsync();
+                rtxMessage.AppendText("Connection started \r\n");
+
+                await connection.InvokeAsync("AddToGroup",
+                    userName, group);
+
+                btnConnect.IsEnabled = false;
+                btnDisConnect.IsEnabled =
+                btnSendMsg.IsEnabled =
+                btnSendToSystem.IsEnabled =
+                btnSendToGroup.IsEnabled = true;
+            }
+            catch (Exception ex)
+            {
+                rtxMessage.AppendText(ex.Message);
+            }
+
+            rtxMessage.ScrollToEnd();
+        }
+
+        private async void btnSendMsg_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                await connection.InvokeAsync("SendMessage",
+                    userName, txtSendMsg.Text);
+            }
+            catch (Exception ex)
+            {
+                rtxMessage.AppendText(ex.Message + "\r\n");
+            }
+
+            rtxMessage.ScrollToEnd();
+        }
+
+        private async void btnDisConnect_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                await connection.InvokeAsync("RemoveFromGroup",
+                    userName, group);
+
+                await connection.StopAsync();
+                rtxMessage.AppendText("Connection stoped \r\n");
+                btnConnect.IsEnabled = true;
+                btnDisConnect.IsEnabled = 
+                btnSendMsg.IsEnabled =
+                btnSendToSystem.IsEnabled =
+                btnSendToGroup.IsEnabled = false;
+            }
+            catch (Exception ex)
+            {
+                rtxMessage.AppendText(ex.Message);
+            }
+
+            rtxMessage.ScrollToEnd();
+        }
+
+        private async void btnSendToGroup_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                await connection.InvokeAsync("SendToGroupMessage",
+                    userName, txtSendMsg.Text, group);
+            }
+            catch (Exception ex)
+            {
+                rtxMessage.AppendText(ex.Message + "\r\n");
+            }
+
+            rtxMessage.ScrollToEnd();
+        }
+
+        private async void btnSendToSystem_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                await connection.InvokeAsync("SendToCallerMessage",
+                    userName, txtSendMsg.Text);
+            }
+            catch (Exception ex)
+            {
+                rtxMessage.AppendText(ex.Message + "\r\n");
+            }
+
+            rtxMessage.ScrollToEnd();
+        }
+    }
+}
